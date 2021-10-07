d031588e29a0a52631aab1059aa4b69162a6f470
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2021-10-07 21:40:03 +0700

    setup data siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,4 +7,45 @@
 module.exports = {
   /* Your site config here */
   plugins: [`gatsby-plugin-sass`],
+  siteMetadata: {
+    title: "Ibrahim Tarigan",
+    description: "Ibrahim Tarigan Portofolio Front end developer",
+    copyright: "Copyright (c) 2021 by Ibrahim Tarigan",
+    projects: [
+      {
+        title: "Talky",
+        description: "Talky Chat app using React-Native and Nest",
+        stacks: ["react-native", "nest"],
+        link: "http://talky.ibrahimtarigan.me",
+        repo: "",
+        repoPrivate: false,
+      },
+      {
+        title: "My Route",
+        description: "Talky Chat app using React-Native and Nest",
+        stacks: ["react-native", "nest"],
+        link: "http://myroute.ibrahimtarigan.me",
+        repo: "",
+        repoPrivate: false,
+      },
+    ],
+    technologies: [
+      {
+        icon: "",
+        name: "",
+        level: "",
+      },
+    ],
+    about: {
+      avatar: "",
+      profile: "",
+    },
+    contacts: [
+      {
+        icon: "",
+        name: "Email",
+        value: "ibrahimtarigan@gmail.com",
+      },
+    ],
+  },
 };
