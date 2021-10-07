9293d255b5c987f14569ca31165954c98f2d3de8
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2021-10-07 23:29:08 +0700

    update project pages > show project list

--- src/pages/project.js
+++ src/pages/project.js
@@ -1,32 +1,34 @@
 import React from "react";
+import { graphql, useStaticQuery } from "gatsby";
 import Layout from "../components/Layout";
+import ProjectList from "../components/ProjectList";
 
 export default function Project() {
+  const data = useStaticQuery(graphql`
+    query SiteProjects {
+      site {
+        siteMetadata {
+          projects {
+            image
+            description
+            link
+            repo
+            repoPrivate
+            stacks
+            title
+          }
+        }
+      }
+    }
+  `);
+
+  const { projects } = data.site.siteMetadata;
+
   return (
     <Layout>
-      <section>
-        My Projects:
-        <ul>
-          <li>
-            <a
-              href="http://talky.ibrahimtarigan.me"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Talky Server
-            </a>
-          </li>
-          <li>
-            <a
-              href="http://myroute.ibrahimtarigan.me"
-              target="_blank"
-              rel="noreferrer"
-            >
-              My Route
-            </a>
-          </li>
-        </ul>
-      </section>
+      <div className="project">
+        <ProjectList projects={projects} />
+      </div>
     </Layout>
   );
 }
