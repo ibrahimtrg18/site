6471d90a4b36fdb1bf9e1ac6446330f47ffc13d4
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2021-10-08 21:26:26 +0700

    update card component > improving reusable for about me

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,24 +1,42 @@
 import React from "react";
+import classNames from "classnames";
 import "./style.scss";
 
-const Card = ({ data }) => {
+const Card = props => {
+  const { data, reverse, itemsCenter, imagePadding } = props;
+
+  const cardClass = classNames({
+    card: true,
+    reverse: reverse && reverse,
+    "card--items-center": itemsCenter && itemsCenter,
+  });
+
   return (
-    <article className="card">
-      <div className="card__image">
+    <article className={cardClass}>
+      <div
+        className="card__image"
+        style={{
+          padding: imagePadding && `${imagePadding.x}px ${imagePadding.y}px`,
+        }}
+      >
         <img src={data.image} alt={data.title} />
       </div>
       <div className="card__content">
         <div className="card__title">{data.title}</div>
         <div className="card__description">{data.description}</div>
         <div className="card__time"></div>
-        <div className="card__stack">
-          {data.stacks.map(stack => (
-            <div className="badge">{stack}</div>
-          ))}
-        </div>
-        <div className="card__link">
-          <a href={data.link}>Preview</a>
-        </div>
+        {data.stack && (
+          <div className="card__stack">
+            {data.stacks.map(stack => (
+              <div className="badge">{stack}</div>
+            ))}
+          </div>
+        )}
+        {data.link && (
+          <div className="card__link">
+            <a href={data.link}>Preview</a>
+          </div>
+        )}
       </div>
     </article>
   );
