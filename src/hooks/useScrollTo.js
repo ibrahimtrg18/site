056d856e804c6d8c1f7733b5467e01a4527580cf
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2021-10-16 22:54:17 +0700

    fix navigation

--- src/hooks/useScrollTo.js
+++ src/hooks/useScrollTo.js
@@ -1,7 +1,7 @@
-import { useLayoutEffect } from "react";
+import { useEffect } from "react";
 
 export const useScrollTo = id => {
-  useLayoutEffect(() => {
+  useEffect(() => {
     if (id) {
       const el = document.getElementById(id);
       const top = window.scrollY + el.getBoundingClientRect().top;
