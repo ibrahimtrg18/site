c0b755594276e4b30011ac88eca06962c6c54489
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2021-11-21 18:27:49 +0700

    edit: profile content not use from card

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,17 +1,29 @@
 import React from "react";
-import Card from "../Card";
 import "./style.scss";
 
 const Profile = ({ profile }) => {
   return (
     <section id="about" className="profile">
-      <Card
-        data={profile}
-        reverse
-        itemsCenter
-        imagePadding={{ x: 16, y: 16 }}
-        imageFull
-      />
+      <div className="profile__content">
+        <div className="card__content">
+          <div className="card__title">{profile.title}</div>
+          <div className="card__description">{profile.description}</div>
+          <div className="card__time"></div>
+          {profile.stack && (
+            <div className="card__stack">
+              {profile.stacks.map(stack => (
+                <div className="badge">{stack}</div>
+              ))}
+            </div>
+          )}
+          {profile.link && (
+            <div className="card__link">
+              <a href={profile.link}>Preview</a>
+            </div>
+          )}
+        </div>
+        <img src={profile.image} alt={profile.title} />
+      </div>
     </section>
   );
 };
