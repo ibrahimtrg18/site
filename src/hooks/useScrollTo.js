f4ee97086fdeec2fcfe49125697ca30cf337a284
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2021-11-21 20:14:02 +0700

    fix scrollTo

--- src/hooks/useScrollTo.js
+++ src/hooks/useScrollTo.js
@@ -4,7 +4,7 @@ export const useScrollTo = id => {
   useEffect(() => {
     if (id) {
       const el = document.getElementById(id);
-      const top = window.scrollY + el.getBoundingClientRect().top;
+      const top = window.scrollY + el.getBoundingClientRect().top - 60;
       window.scrollTo({ top, behavior: "smooth" });
     }
   }, [id]);
