bfe785191d6d9969eefa872e5984475a62af4462
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2022-03-13 13:19:37 +0700

    setup: gatsby-image

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,11 @@
+const path = require("path");
+
 /** @type {import('gatsby').GatsbyConfig} */
 module.exports = {
   siteMetadata: {
     title: "Ibrahim Tarigan",
     titleTemplate: "%s · Enthusiast Frontend Developer",
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: "https://www.yourdomain.tld",
     description:
       "Ibrahim Tarigan, a dude love and passion in frontend development, and want to be great frontend developer",
     url: "https://www.ibrahimtarigan.me",
@@ -17,5 +19,16 @@ module.exports = {
       link: "",
     },
   },
-  plugins: ["gatsby-plugin-styled-components"],
+  plugins: [
+    "gatsby-plugin-styled-components",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: path.join(__dirname, "src", "images"),
+      },
+    },
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
+  ],
 };
