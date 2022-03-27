e25d79c1e8a20c58db4b8f51951e64f0d19c58eb
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2022-03-27 10:49:05 +0700

    feature: add toggle props to Navbar for showing or hide by props navbar

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -8,19 +8,24 @@ import Navbar from "./Navbar";
 
 interface Props {
   children: React.ReactNode;
+  navbar: boolean;
 }
 
 const Layout: FC<Props> = (props) => {
-  const { children } = props;
+  const { children, navbar } = props;
 
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <SEO />
-      <Navbar />
+      {navbar && <Navbar />}
       <Main>{children}</Main>
     </ThemeProvider>
   );
+};
+
+Layout.defaultProps = {
+  navbar: true,
 };
 
 export default Layout;
