99c1772281e6eabdfc93944753b624d86e23decc
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2022-03-27 10:34:08 +0700

    feature: add Navbar and Main to Layout component

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -3,16 +3,22 @@ import { ThemeProvider } from "styled-components";
 
 import { GlobalStyle, theme } from "../../config/styled-components";
 import SEO from "../SEO";
+import Main from "./Main";
+import Navbar from "./Navbar";
 
 interface Props {
   children: React.ReactNode;
 }
 
-const Layout: FC<Props> = () => {
+const Layout: FC<Props> = (props) => {
+  const { children } = props;
+
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <SEO />
+      <Navbar />
+      <Main>{children}</Main>
     </ThemeProvider>
   );
 };
