f598f0f9846ad53cfee9a6b010fe4302cbfd9e92
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2022-03-27 10:28:02 +0700

    feature: change fonts

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -41,7 +41,7 @@ const SEO: FC<Props> = ({ title, description, image, article }) => {
         crossOrigin="true"
       />
       <link
-        href="https://fonts.googleapis.com/css2?family=Arvo:wght@400;700&family=Assistant:wght@200;300;400;500;600;700;800&display=swap"
+        href="https://fonts.googleapis.com/css2?family=Assistant:wght@200;300;400;500;600;700;800&family=Lora:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap"
         rel="stylesheet"
       />
       <meta name="description" content={seo.description} />
