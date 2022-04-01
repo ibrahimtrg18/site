cc4ddb38975baf1d28df5d5a21528bcab5722404
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2022-04-01 13:14:19 +0700

    feat: improving ui/ux for navbar
    add sidebar, responsive, data dynamic

--- src/components/Layout/Navbar/index.tsx
+++ src/components/Layout/Navbar/index.tsx
@@ -1,8 +1,9 @@
+import classNames from "classnames";
 import { graphql, useStaticQuery } from "gatsby";
-import React, { useMemo } from "react";
-import * as Icon from "react-feather";
+import React, { useMemo, useState } from "react";
 
 import { getInitial } from "../../../helpers/common";
+import { Icon, IconName } from "../../base/Icon";
 import {
   AppBar,
   Logo,
@@ -11,41 +12,54 @@ import {
   MenuNav,
   NavbarContainer,
   Sidebar,
-  SocialMediaItem,
   SocialMediaList,
 } from "./styled";
+
+interface SocialMedia {
+  iconName: IconName;
+  username: string;
+  link: string;
+}
 
 const Navbar = () => {
   const data = useStaticQuery(query);
   const logo = useMemo(() => getInitial(data.site.siteMetadata.title), []);
+  const socialMedias: Array<SocialMedia> = useMemo(
+    () => data.site.siteMetadata.socialMedias,
+    []
+  );
   const menus = useMemo(() => ["About", "Project", "Resume"], []);
+  const [showSidebarMenu, setShowSidebarMenu] = useState(false);
 
   const renderMenu = () =>
     menus.map((menu) => <MenuItem key={menu}>{menu}</MenuItem>);
 
+  const renderSocialMedia = () =>
+    socialMedias.map((social, i) => <Icon key={i} name={social.iconName} />);
+
   return (
     <NavbarContainer>
       <AppBar>
+        <Icon
+          name="Menu"
+          mx="16px"
+          className={classNames("menu")}
+          onClick={() => setShowSidebarMenu(true)}
+        />
         <MenuNav>
           <MenuList>{renderMenu()}</MenuList>
         </MenuNav>
         <Logo>{logo}</Logo>
-        <SocialMediaList>
-          <SocialMediaItem>
-            <Icon.Twitter />
-          </SocialMediaItem>
-          <SocialMediaItem>
-            <Icon.Instagram />
-          </SocialMediaItem>
-          <SocialMediaItem>
-            <Icon.GitHub />
-          </SocialMediaItem>
-          <SocialMediaItem>
-            <Icon.Linkedin />
-          </SocialMediaItem>
-        </SocialMediaList>
+        <SocialMediaList>{renderSocialMedia()}</SocialMediaList>
       </AppBar>
-      <Sidebar></Sidebar>
+      <Sidebar show={showSidebarMenu}>
+        <MenuList>{renderMenu()}</MenuList>
+        <Icon
+          name="X"
+          className="close-menu"
+          onClick={() => setShowSidebarMenu(false)}
+        />
+      </Sidebar>
     </NavbarContainer>
   );
 };
@@ -57,6 +71,11 @@ const query = graphql`
     site {
       siteMetadata {
         title
+        socialMedias {
+          iconName
+          username
+          link
+        }
       }
     }
   }
--- src/components/Layout/Navbar/styled.ts
+++ src/components/Layout/Navbar/styled.ts
@@ -1,35 +1,114 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const NavbarContainer = styled.header``;
 
 export const AppBar = styled.div`
-  position: fixed;
-  top: 0;
-  display: flex;
-  width: 100%;
-  justify-content: space-between;
+  position: relative;
+  display: grid;
   align-items: center;
-  height: 124px;
-  background-color: transparent;
+  width: 100%;
+  height: 64px;
+  background-color: #ffffff;
+  box-shadow: 0 0 2px 0 rgba(0, 0, 0, 0.1);
+
+  .menu {
+    position: absolute;
+    display: block;
+
+    @media ${(props) => props.theme.device.mobileL} {
+      display: none;
+    }
+  }
+
+  @media ${(props) => props.theme.device.mobileL} {
+    display: grid;
+    grid-template-columns: auto 1fr auto;
+    grid-auto-flow: column;
+    justify-content: space-between;
+    align-items: center;
+    background-color: transparent;
+  }
+  @media ${(props) => props.theme.device.tablet} {
+    z-index: 1;
+    position: fixed;
+    top: 0;
+    display: grid;
+    grid-template-columns: auto auto;
+    grid-auto-flow: column;
+    align-items: center;
+    height: 124px;
+    background-color: transparent;
+    box-shadow: unset;
+  }
 `;
 
-export const Sidebar = styled.nav``;
-
 export const MenuNav = styled.nav`
-  margin: 48px 0 48px 96px;
+  display: none;
+
+  @media ${(props) => props.theme.device.mobileL} {
+    display: block;
+    margin: 0 16px;
+  }
+  @media ${(props) => props.theme.device.tablet} {
+    margin: 24px 0 24px 96px;
+  }
+  @media ${(props) => props.theme.device.laptop} {
+    margin: 48px 0 48px 96px;
+  }
 `;
 
 export const MenuList = styled.ul`
   list-style: none;
   display: flex;
-  gap: 4.5em;
+  gap: 2em;
+
+  @media ${(props) => props.theme.device.tablet} {
+    gap: 3em;
+  }
+  @media ${(props) => props.theme.device.laptop} {
+    gap: 4.5em;
+  }
 `;
 
 export const MenuItem = styled.li`
+  position: relative;
   font-family: ${(props) => props.theme.font.Lora};
-  font-size: ${(props) => props.theme.fontSize.menu};
-  font-weight: 700;
+  font-size: ${(props) => props.theme.fontSize.menuSm};
+  font-weight: 400;
   cursor: pointer;
+
+  &:after {
+    content: "";
+    position: absolute;
+    left: 0;
+    bottom: -10px;
+    width: 0;
+    height: 2px;
+    background-color: ${(props) => props.theme.color.text};
+    transition: width 0.2s ease-in-out;
+  }
+
+  &:hover {
+    &:after {
+      margin-right: 8px;
+      width: calc(100% - 8px);
+
+      @media ${(props) => props.theme.device.mobileL} {
+        bottom: -5px;
+        width: 100%;
+      }
+    }
+  }
+
+  @media ${(props) => props.theme.device.mobileL} {
+    font-size: ${(props) => props.theme.fontSize.menuSm};
+  }
+  @media ${(props) => props.theme.device.tablet} {
+    font-size: ${(props) => props.theme.fontSize.menuSm};
+  }
+  @media ${(props) => props.theme.device.laptop} {
+    font-size: ${(props) => props.theme.fontSize.menuLg};
+  }
 `;
 
 export const Logo = styled.h1`
@@ -38,16 +117,140 @@ export const Logo = styled.h1`
   top: 50%;
   transform: translate(-50%, -50%);
   font-family: ${(props) => props.theme.font.Lora};
-  font-size: ${(props) => props.theme.fontSize.logo};
+  font-size: ${(props) => props.theme.fontSize.logoSm};
+
+  @media ${(props) => props.theme.device.mobileL} {
+    position: unset;
+    transform: unset;
+    font-size: ${(props) => props.theme.fontSize.logoMd};
+    margin: 0 16px;
+    grid-column-start: 1;
+  }
+  @media ${(props) => props.theme.device.tablet} {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    font-size: ${(props) => props.theme.fontSize.logoLg};
+    margin: 0;
+  }
 `;
 
 export const SocialMediaList = styled.ul`
   list-style: none;
-  display: flex;
-  margin: 48px 96px 48px 0;
-  gap: 4.5em;
+  display: none;
+
+  & > svg {
+    width: 18px;
+    height: 18px;
+  }
+
+  @media ${(props) => props.theme.device.mobileL} {
+    display: flex;
+    gap: 2em;
+    margin-right: 16px;
+  }
+  @media ${(props) => props.theme.device.tablet} {
+    display: flex;
+    gap: 3em;
+    justify-content: space-between;
+    align-items: center;
+    background-color: transparent;
+    margin: 24px 96px 24px 0;
+
+    & > svg {
+      width: 24px;
+      height: 24px;
+    }
+  }
+  @media ${(props) => props.theme.device.laptop} {
+    gap: 4.5em;
+    margin: 48px 96px 48px 0;
+  }
 `;
 
 export const SocialMediaItem = styled.li`
   display: inline-flex;
+`;
+
+interface SidebarProps {
+  show: boolean;
+}
+
+export const Sidebar = styled.nav<SidebarProps>`
+  ${(props) => {
+    if (props.show) {
+      return css`
+        left: 0;
+        transition: left 0.5s ease-in-out;
+      `;
+    } else {
+      return css`
+        left: -100%;
+        transition: left 1s ease-in-out;
+      `;
+    }
+  }};
+  position: absolute;
+  top: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: #ffffff;
+  box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2);
+
+  & > ${MenuList} {
+    display: flex;
+    height: 100vh;
+    /* height: calc(100vh - 40%); */
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 4em;
+
+    & > ${MenuItem} {
+      position: relative;
+      font-size: ${(props) => props.theme.fontSize.menuXl};
+      letter-spacing: 8px;
+      text-transform: uppercase;
+      ${(props) => {
+        if (props.show) {
+          return css`
+            opacity: 1;
+            visibility: visible;
+            transform: translateY(0);
+            transition: all 1s ease-in-out;
+          `;
+        } else {
+          return css`
+            opacity: 0;
+            visibility: hidden;
+            transform: translateY(-25px);
+            transition: all 0.25s ease-in-out;
+          `;
+        }
+      }};
+    }
+  }
+
+  & > .close-menu {
+    position: absolute;
+    top: 1em;
+    right: 1em;
+
+    ${(props) => {
+      if (props.show) {
+        return css`
+          opacity: 1;
+          visibility: visible;
+          transition: all 0.5s ease-in-out;
+        `;
+      } else {
+        return css`
+          opacity: 0;
+          visibility: hidden;
+          transition: all 0.25s ease-in-out;
+        `;
+      }
+    }};
+  }
 `;
