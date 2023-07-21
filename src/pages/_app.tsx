b52896b92de0f34dfc3ae3b725bd72ad9c0bc689
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2023-07-21 18:43:45 +0700

    feat: add elegance to root app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
+import { Elegance } from "@ibrahimtrg18/elegance";
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Elegance>
+      <Component {...pageProps} />
+    </Elegance>
+  );
 }
 
 export default MyApp;
