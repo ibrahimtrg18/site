e3e5a4f607d63bded0434c3dfec75aa6dcd0e3b8
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2023-08-23 23:16:54 +0700

    feat: improving metadata

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -7,12 +7,12 @@ import About from "./components/About";
 import Me from "./components/Me";
 import Technology from "./components/Technology";
 
+export const revalidate = 3600;
 export const metadata: Metadata = {
-  title: "Me | Ibrahim Tarigan ",
-  description: "Me Page",
+  title: "Home | Ibrahim Tarigan",
+  description:
+    "Hello, I'm Ibrahim Tarigan aka Baim, a passionate Software Engineer with a track record of bringing digital ideas to life. With 2 years of hands-on experience, I specialize in designing, coding, testing, and debugging applications that make an impact. My expertise spans web and mobile development, and I'm dedicated to crafting efficient, user-friendly solutions that exceed expectations. As a collaborative team player, I thrive in cross-functional environments and constantly seek opportunities to expand my technical prowess. Join me on my journey of turning visions into reality.",
 };
-
-// export const revalidate = 3600;
 
 export default async function MePage() {
   const {
--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -1,9 +1,16 @@
+import { Metadata } from "next";
+
 import { Container } from "../../components/Container";
 import { Section } from "../../components/Section";
 import { getProjects } from "../../graphql/queries/project";
 import { ProjectList } from "./components/ProjectList";
 
-// export const revalidate = 3600;
+export const revalidate = 3600;
+export const metadata: Metadata = {
+  title: "Project | Ibrahim Tarigan",
+  description:
+    "Explore a diverse collection of projects I've passionately worked on and contributed to. From web development to innovative solutions, discover my journey through hands-on experiences and creative contributions.",
+};
 
 export default async function ProjectPage() {
   const {
