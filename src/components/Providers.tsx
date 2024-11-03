d7565b6bcd14b256f0f048856681064263278d43
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2024-11-03 22:23:53 +0700

    feature: remove unused code

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -6,7 +6,6 @@ import { MDXProvider } from "@mdx-js/react";
 
 import { AppProvider } from "@/contexts/AppContext/AppContext";
 import { App } from "@/generated/graphql";
-// import { ConfigurationContextProvider } from "@/contexts/ConfigurationContext/ConfigurationContext";
 import { ApolloProvider } from "@/libs/apollo/client";
 import { theme } from "@/theme";
 import { MDXComponents } from "@/theme/MDXComponents";
@@ -23,11 +22,7 @@ export const Providers = ({ children, app }: ProvidersProps) => {
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <ChakraProvider theme={theme}>
         <MDXProvider components={MDXComponents}>
-          <AppProvider app={app}>
-            {/* <ConfigurationContextProvider configuration={configuration}> */}
-            {children}
-            {/* </ConfigurationContextProvider> */}
-          </AppProvider>
+          <AppProvider app={app}>{children}</AppProvider>
         </MDXProvider>
       </ChakraProvider>
     </ApolloProvider>
