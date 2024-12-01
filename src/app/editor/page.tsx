58e7b60b060951857f1fee912119bcdeb34aa3cd
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2024-12-01 22:25:15 +0700

    chore: remove unused code

--- src/app/editor/page.tsx
+++
@@ -1,8 +0,0 @@
-import { getPage } from "@/graphql/api/getPage";
-import { PageEditor } from "@/views/PageEditor";
-
-export default async function EditorPage() {
-  const { data: pageData } = await getPage({ path: "/" });
-
-  return <PageEditor page={pageData.page} />;
-}
