e2aeb3ef406cd072f5d778ab14fa2c83c2b81b90
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2024-12-22 22:27:34 +0700

    feature: add MDX support and update page extensions in Next.js config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,13 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+import mdx from "@next/mdx";
+
 const isProd = process.env.NODE_ENV === "production";
+const withMDX = mdx();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
   env: {
     SITE_URL: process.env.SITE_URL,
     HYGRAPH_GRAPHQL_URI: process.env.HYGRAPH_GRAPHQL_URI,
@@ -20,4 +24,4 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+export default withMDX(nextConfig);
