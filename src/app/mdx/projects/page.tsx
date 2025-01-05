9697d5c31a302e6d4c1f2602e1666cbc4d693cdf
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2025-01-05 20:27:44 +0700

    feature: add Projects page and ProjectCard component with metadata for project descriptions

---
+++ src/app/mdx/projects/page.tsx
@@ -0,0 +1,28 @@
+import { Flex } from "@chakra-ui/react";
+import fs from "fs";
+import path from "path";
+
+import { ProjectCard } from "@/components";
+
+export { metadata } from "@/markdown/projects.mdx";
+
+export default async function ProjectsPage() {
+  const folderPath = path.join(process.cwd(), "src", "markdown", "projects"); // Absolute path to the folder
+  const files = fs.readdirSync(folderPath); // Read folder contents
+
+  const projects = await Promise.all(
+    files.map(async (fileName) => {
+      const { metadata } = await import(`@/markdown/projects/${fileName}`);
+
+      const slug = `/mdx/projects/${fileName.replace(/\.mdx?$/, "")}`; // @TODO remove /mdx
+
+      return <ProjectCard key={metadata.title} {...metadata} href={slug} />;
+    })
+  );
+
+  return (
+    <Flex direction="column" gap={2}>
+      {projects}
+    </Flex>
+  );
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
+export * from "./Card";
 export { default as Container } from "./Container";
 export { default as Layout } from "./Layout";
 export { default as SocialMedia } from "./SocialMedia";
